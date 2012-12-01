289afe0ec247fb12393990d63b3eb6f41018610e
Loic Sharma <sharma.loic@gmail.com>
2012-12-01 01:35:56 -0500

    Improved the renderer.

--- src/Menu/Renderer.php
+++ src/Menu/Renderer.php
@@ -4,11 +4,11 @@ namespace menu;
 
 class Renderer {
 
-	public $classes = array();
+	public $options = array();
 
 	public function __construct()
 	{
-		$this->classes = array(
+		$this->options = array(
 			'class.first'   => 'first',
 			'class.last'    => 'last',
 			'class.single'  => 'single',
@@ -19,90 +19,80 @@ class Renderer {
 	{
 		$output = '';
 
-		foreach($menu->items() as $item)
+		$items = $menu->items();
+		$last = count($items)-1;
+
+		foreach($items as $key => $item)
 		{
-			$output .= $this->renderItem($item);
+			if($key == 0)
+			{
+				$item['li.class'] = $this->options['class.first'];
+			}
+
+			elseif($key == $last)
+			{
+				$item['li.class'] = $this->options['class.last'];
+			}
+
+			$output .= $this->renderItem($item, 1);
 		}
 
 		return $output;
 	}
 
-	public function renderItem($item)
+	public function renderItem($item, $depth = 1)
 	{
-		$output = '<li'.$this->attributes($item, 'li').'>';
+		$output = $this->format('<li'.$this->attributes($item, 'li').'>', $depth);
 
 		if( ! empty($item['a']))
 		{
-			$output .= '<a'.$this->attributes($item, 'a').'>';
-			$output .= $item->name.'</a>';
+			$link = '<a'.$this->attributes($item, 'a').'>'.$item->name.'</a>';
+
+			$output .= $this->format($link, $depth+1);
 		}
 
 		else
 		{
-			$output .= $item->name;
+			$output .= $this->format($item->name, $depth+1);
 		}
 
-		$output .= $this->renderList($item);
-
-		$output .= '</li>'.PHP_EOL;
+		$output .= $this->renderList($item, $depth+1);
+		$output .= $this->format('</li>', $depth);
 
 		return $output;
 	}
 
-	public function renderList($list, $nested = true)
+	public function renderList($list, $depth = 1)
 	{
-		$items = $list->items();
-
 		$output = '';
+
+		$items = $list->items();
 
 		if( ! empty($items))
 		{
-			$first = true;
-			$keys = count($items);
+			$output = $this->format('<ul'.$this->attributes($list, 'ul').'>', $depth);
+
+			$itemCount = count($items);
 
 			foreach($items as $key => $item)
 			{
-				if($keys == 1 && $nested == true)
+				if($itemCount == 1)
 				{
-					$item['class'] = $this->classes['class.single'];
+					$item['li.class'] = $this->options['class.single'];
 				}
 
-				elseif($first == true)
-				{
-					$item['class'] = $this->classes['class.first'];
-
-					$first = false;
-				}
-
-				elseif($nested == false && $key == $keys-1)
-				{
-					$item['class'] = $this->classes['class.last'];
-				}
-
-				$output .= $this->renderItem($item);
-			}
-		}
-
-		if($nested == false || ! empty($items))
-		{
-			$itemsOutput = $output;
-			$output = '';
-
-			if($nested == true)
-			{
-				$output  = PHP_EOL;
-				$output .= '<ul'.$this->attributes($list, 'ul').'>'.PHP_EOL;
+				$output .= $this->renderItem($item, $depth+1);
 			}
 
-			else
-			{
-				$output .= '<ul'.$this->attributes($list, 'ul').'>'.PHP_EOL;
-			}
-
-			$output .= $itemsOutput.'</ul>'.PHP_EOL;
+			$output .= $this->format('</ul>', $depth);
 		}
 
 		return $output;
+	}
+
+	protected function format($html, $depth)
+	{
+		return str_repeat(' ', $depth * 4).$html.PHP_EOL;
 	}
 
 	protected function attributes($item, $type)
